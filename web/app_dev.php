0d34a43203171df689492a8b60b399e3d71d9aa1
Makoa Jacobsen <makoa@makoajacobsen.com>
2017-07-16 19:29:24 -0700

    Update initial files.

--- web/app_dev.php
+++ web/app_dev.php
@@ -9,34 +9,39 @@
  * File that kicks it all off!
  */
 
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
+// If you don't want to setup permissions the proper way, just uncomment the following PHP line
+// read https://symfony.com/doc/current/setup.html#checking-symfony-application-configuration-and-setup
+// for more information
+//umask(0000);
+
+// This check prevents access to debug front controllers that are deployed by accident to production servers.
+// Feel free to remove this, extend it, or make something more sophisticated.
+/*
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@substr($_SERVER['REMOTE_ADDR'], 0, 3), array('10.', '192.168.')) || php_sapi_name() === 'cli-server')
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'], true) || PHP_SAPI === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
+*/
 
 /**
  * Root directory of Kula SIS.
  */
 define('KULA_ROOT', dirname(getcwd()));
 
-$loader = require_once __DIR__.'/../app/autoload.php';
-
+require __DIR__.'/../vendor/autoload.php';
 Debug::enable();
 
-require_once __DIR__.'/../app/AppKernel.php';
-
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
-
+if (PHP_VERSION_ID < 70000) {
+    $kernel->loadClassCache();
+}
 $request = Request::createFromGlobals();
-
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
